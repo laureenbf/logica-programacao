9327215b6a8c267dad751d9ddca8fb2813078462
laureenbf <laureenbarros2@gmail.com>
2026-09-12 07:57:55 -0300

    Add files via upload
    códigos das primeiras aulas de laços de repetição

---
+++ repeticao.py
@@ -0,0 +1,10 @@
+valor = int(input("Digite o valor da tabuada: "))
+
+i = 1
+
+while i <= 10:
+    resultado = valor * i
+    print (valor, "x", i, " = ", resultado)
+    i = i + 1
+
+    
---
+++ soma5.py
@@ -0,0 +1,11 @@
+i = 1
+soma = 0
+
+while i <= 5:
+    numero = int(input("digite o número"))
+    soma += numero
+    i += 1
+
+    
+
+print(soma)
